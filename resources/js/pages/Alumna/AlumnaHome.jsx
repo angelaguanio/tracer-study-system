ce5cc4a2aa665dc7b1042150a34dcefd6e30f8e6
angelaguanio <angelaguanio@gmail.com>
2026-01-17 19:41:53 +0800

    fixed navbar
    added layout to contain navbar, also fixed the missing column

--- resources/js/pages/Alumna/AlumnaHome.jsx
+++ resources/js/pages/Alumna/AlumnaHome.jsx
@@ -4,15 +4,16 @@ import React from 'react'
 import { Button } from '../../components/ui/button';
 import { Link } from '@inertiajs/react';
 import NavbarAlumni from '../../components/navbar-alumni';
+import AlumnaLayout from '../../layouts/alumna-layout';
 
 
 
 export default function AlumnaHome() {
   return (
     <>
-      <NavbarAlumni>
-        {/* content */}
-      </NavbarAlumni>
-  </>
+      <AlumnaLayout>
+        
+      </AlumnaLayout>
+    </>
   )
 }
