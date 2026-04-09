4b5bc066c4b39fc7bb4824971306be7a5b004c49
Reygie Allapitan <egieallapitan@gmail.com>
2026-04-09 19:45:55 +0800

    ewan ko bat mali nakalagay dyan, now ko lng napansin haha

--- resources/js/pages/Alumna/AlumnaAnnouncements.jsx
+++ resources/js/pages/Alumna/AlumnaAnnouncements.jsx
@@ -42,4 +42,4 @@ export default function AlumnaAnnouncements() {
   );
 }
 
-AlumnaAssociation.layout = page => <AlumnaLayout>{page}</AlumnaLayout>;
+AlumnaAnnouncements.layout = page => <AlumnaLayout>{page}</AlumnaLayout>;
