e6ed44d70d871dcbef12c611869bdf5bdba1dabc
angelaguanio <angelaguanio@gmail.com>
2026-04-17 16:11:43 +0800

    Update AlumnaOffice.jsx

--- resources/js/pages/Alumna/AlumnaOffice.jsx
+++ resources/js/pages/Alumna/AlumnaOffice.jsx
@@ -8,8 +8,8 @@ function AlumnaOffice() {
     <div className="min-h-screen pt-20">
 
       {/* PAGE TITLE */}
-      <div className="text-center py-6">
-        <h1 className="text-4xl md:text-6xl font-bold text-blue-800">
+      <div className="text-center pb-20">
+        <h1 className="text-4xl md:text-7xl font-bold text-blue-800 font-inria">
           ALUMNI AFFAIRS
         </h1>
         <p className="text-xl md:text-2xl text-blue-700 mt-2">
