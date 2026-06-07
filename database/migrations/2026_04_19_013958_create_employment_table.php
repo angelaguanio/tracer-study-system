a90ccb385a18c9399eddfda280632c33dc491fb3
angelaguanio <angelaguanio@gmail.com>
2026-06-07 20:34:01 +0800

    add dept and courses
    -kulang pa ung programs, for revision ulit pero functioning nnaman na

--- database/migrations/2026_04_19_013958_create_employment_table.php
+++ database/migrations/2026_04_19_013958_create_employment_table.php
@@ -20,7 +20,7 @@ return new class extends Migration
             $table->string('position')->nullable();
             $table->string('location')->nullable();
 
-            $table->smallInteger('employment_start_year'); // smallInteger is sufficient for a 4-digit year
+            $table->smallInteger('employment_start_year')->nullable(); // smallInteger is sufficient for a 4-digit year
             $table->smallInteger('employment_end_year')->nullable();
             $table->boolean('is_current')->default(false);
 
