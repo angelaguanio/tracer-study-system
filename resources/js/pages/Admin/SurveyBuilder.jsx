05388e9b2128896726ad4b0a4740eea2c628137c
angelaguanio <angelaguanio@gmail.com>
2026-06-10 13:11:29 +0800

    Update SurveyBuilder.jsx

--- resources/js/pages/Admin/SurveyBuilder.jsx
+++ resources/js/pages/Admin/SurveyBuilder.jsx
@@ -1,4 +1,4 @@
-import  {route}  from "ziggy-js";
+import { route } from 'vendor/tightenco/ziggy';
 import { useState, useEffect } from "react";
 import { router, usePage, Link, useForm } from "@inertiajs/react";
 import { Plus, ArrowLeft, Pencil, Check } from "lucide-react";
